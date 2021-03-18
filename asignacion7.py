55d82eacbe715012d5e51908f5caaa2a17127fcf
Daniel Malaver <14-10605@usb.ve>
2021-03-18 13:04:33 -0400

    Actualizacion en Rama

--- asignacion7.py
+++ asignacion7.py
@@ -6,3 +6,13 @@ response = requests.post(
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
 pprint(payload)
+
+response2 = requests.get(
+    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+    headers={'X-Auth-Token':payload['Token']})
+payload2 = response2.json()['response']
+nameList = []
+for i in range(len(payload2)):
+    nameList.append([payload2[i]['family'],payload2[i]['hostname'],payload2[i]['managementIpAddress'],payload2[i]['lastUpdated'],payload2[i]['reachabilityStatus']])
+pprint(nameList)
+
