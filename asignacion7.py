9ca6a5761ae6001a94187d8f1b785f16a3d0b213
DanielMalaver <45952910+DanielMalaver@users.noreply.github.com>
2021-03-18 00:43:10 -0400

    Delete asignacion7.py

--- asignacion7.py
+++
@@ -1,8 +0,0 @@
-import requests
-from pprint import pprint
-
-response = requests.post(
-    'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
-    headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
-payload=response.json()
-pprint(payload)
